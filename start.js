01a6b16ac6bc0708186b4c385c6ea8096cf1f9da
Nepo Aquino <124418583+neppy3250@users.noreply.github.com>
2023-10-12 03:05:21 +0800

    Update start.js

--- start.js
+++ start.js
@@ -1,3 +1,4 @@
+// START GAME BUTTON
 const startGameButton = document.getElementById("startGameButton");
 const header = document.querySelector("header"); 
 const characterSelection = document.getElementById("characterSelection");
@@ -5,4 +6,11 @@ const characterSelection = document.getElementById("characterSelection");
 startGameButton.addEventListener("click", () => {
   header.style.display = "none";
   characterSelection.style.display = "flex";
+});
+
+// Prevent image dragging
+document.addEventListener("dragstart", function(event) {
+  if (event.target.classList.contains("no-drag")) {
+      event.preventDefault();
+  }
 });
