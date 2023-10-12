2147ab5f6de3ce67253b9522c9ff54b879b5339a
Nepo Aquino <124418583+neppy3250@users.noreply.github.com>
2023-10-13 03:49:54 +0800

    Update start.js

--- start.js
+++ start.js
@@ -1,11 +1,13 @@
 // START GAME BUTTON
 const startGameButton = document.getElementById("startGameButton");
+const genderSelectText = document.getElementById("genderSelect");
 const header = document.querySelector("header"); 
 const characterSelection = document.getElementById("characterSelection");
 
 startGameButton.addEventListener("click", () => {
-  header.style.display = "none";
-  characterSelection.style.display = "flex";
+  header.style.display = "none"; // HIDE <header>
+  genderSelectText.style.display = "block"; // Display <h2>
+  characterSelection.style.display = "flex"; // Display <div id="characterSelection">
 });
 
 // Prevent image dragging
